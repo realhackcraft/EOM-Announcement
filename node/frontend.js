249e45bf00f5c4fe713d8ce7b5df81848263945e
Hackcraft_ <hackcraft_@hotmail.com>
2023-03-10 16:36:14 -0500

    Removed useless js files

--- node/frontend.js
+++
@@ -1,6 +0,0 @@
-for (let announcement of announcements) {
-  const announcementDiv = document.createElement('div');
-  announcementDiv.classList.add('announcement');
-  announcementDiv.innerHTML = announcement
-  document.getElementById('announcement-wrapper').appendChild(announcementDiv);
-}
